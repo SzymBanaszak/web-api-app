3f824e912f601386c72bf5215e6ddb6534f63467
szymban837 <szymbanaszak@gmail.com>
2022-03-28 15:06:10 +0200

    return list of users

---
+++ API/my-app-api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using my_app_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace my_app_api.Controllers
+{
+    [RoutePrefix("Users")]
+    public class UsersController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<Users> Get()
+        {
+            using (Entities entities = new Entities())
+            {
+                return entities.Users.ToList();
+            }
+        }
+    }
+}
